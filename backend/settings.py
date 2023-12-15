f40920081e2e24893088cd16097cef2714c7a14d
fazil <fazifazil160160@gmail.com>
2023-12-15 15:53:51 +0530

    cors-headers set

--- backend/settings.py
+++ backend/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "corsheaders",
     'rest_framework',
     'notes',
 ]
@@ -44,6 +45,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -101,6 +103,10 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000'
+]
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
--- notes/views.py
+++ notes/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 from .models import Notes
 from .serializers import NotesSerializer
+
+
+
 # Create your views here.
 
+# this will allow all the functionalities based on the model notes
 class NotesView(ModelViewSet):
     queryset = Notes.objects.all()
     serializer_class = NotesSerializer
